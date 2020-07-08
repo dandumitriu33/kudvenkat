9e7866f5daad5f701d5146cd0d98d7e7437957ed
Dan Dumitriu <dandumitriu33@gmail.com>
2020-07-08 19:50:23 +0300

    Add form validation w/ tag helpers and attributes

--- EmployeeManagement/EmployeeManagement/Models/Employee.cs
+++ EmployeeManagement/EmployeeManagement/Models/Employee.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +12,12 @@ namespace EmployeeManagement.Models
     public class Employee
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(50, ErrorMessage = "Name cannot exceed 50 characters.")]
         public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Office Email")]
         public string Email { get; set; }
         public Dept Department { get; set; }
 
