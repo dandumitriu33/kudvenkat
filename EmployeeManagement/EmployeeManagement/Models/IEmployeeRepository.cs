e2e0c7959bdaf21ee8ed2dc190926ad22782b441
Dan Dumitriu <dandumitriu33@gmail.com>
2020-07-08 19:17:55 +0300

    Add form binding to post method object in parameter

--- EmployeeManagement/EmployeeManagement/Models/IEmployeeRepository.cs
+++ EmployeeManagement/EmployeeManagement/Models/IEmployeeRepository.cs
@@ -9,6 +9,7 @@ namespace EmployeeManagement.Models
     {
         Employee GetEmployee(int Id);
         IEnumerable<Employee> GetAllEmployees();
+        Employee Add(Employee employee);
 
     }
 }
