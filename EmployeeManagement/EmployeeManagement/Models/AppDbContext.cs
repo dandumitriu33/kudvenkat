9a28ba04ad61514b82f2cd808fd20a54fce41d64
Dan Dumitriu <dandumitriu33@gmail.com>
2020-07-09 12:33:31 +0300

    Add Seed mechanism for db

--- EmployeeManagement/EmployeeManagement/Models/AppDbContext.cs
+++ EmployeeManagement/EmployeeManagement/Models/AppDbContext.cs
@@ -13,5 +13,10 @@ namespace EmployeeManagement.Models
         {
 
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Seed();
+        }
     }
 }
